cc35e5aacaabbc7b0dd5c570a49ce8f8f98ac01b
just-random-user <sineokovivan@mail.ru>
2021-04-24 21:35:58 +0300

    Added the project

---
+++ MVC-Test/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _environment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Upload(IFormFile upload)
+        {
+            if(upload!=null)
+            {
+                string fileName = System.IO.Path.GetFileName(upload.FileName);
+                using var fileStream =
+                    new FileStream(_environment.WebRootPath + "\\files\\" + fileName, FileMode.Create);
+                upload.CopyToAsync(fileStream);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+    }
+}
