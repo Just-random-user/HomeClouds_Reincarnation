06b965436f7ed2ed579b39dd5d60caf2d0303ce6
BestSeal <thatlazyseal@gmail.com>
2021-04-25 00:50:17 +0300

    FileUpDoView

---
+++ MVC-Test/Controllers/FileController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.StaticFiles;
+using MVC_Test.ViewModels;
+using System;
+
+namespace MVC_Test.Controllers
+{
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult FileExplorer(FileViewModel file)
+        {
+            if (String.IsNullOrEmpty(file.path))
+            {
+                file = new FileViewModel("D:\\");
+            }
+
+            return View("Index", file);
+        }
+
+        [HttpPost]
+        public IActionResult Download(FileViewModel file)
+        {
+            string path = file.path;
+            string name = Path.GetFileName(path);
+            new FileExtensionContentTypeProvider().TryGetContentType(path, out string contentType);
+            return PhysicalFile(path, contentType, name);
+        }
+    }
+}
---
+++ MVC-Test/ViewModels/FileViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC_Test.ViewModels
+{
+    public class FileViewModel
+    {
+        public string path { get; set; }
+        public string name { get; set; }
+
+        public FileViewModel() { }
+        public FileViewModel(string _path)
+        {
+            path = _path;
+        }
+    }
+}
